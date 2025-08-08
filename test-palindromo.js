9119ed4662d2b341ff6151fa12afa65cf5e65de5
william-0519 <williamac2005@gmail.com>
2025-08-08 12:31:10 -0500

    agregar funcion espalindromo

--- test-palindromo.js
+++ test-palindromo.js
@@ -1,3 +1,10 @@
 function mostrarmensaje(){
-    
+    let entrada = document.getElementById("entrada").value;
+    document.getElementById("texto-salida").innerHTML =
+        esPalindromo(entrada)
+            ? "El texto ingresado es un palindromo."
+            : "El texto ingresado no es un palindromo.";
+}
+
+function esPalindromo(entrada){
 }
