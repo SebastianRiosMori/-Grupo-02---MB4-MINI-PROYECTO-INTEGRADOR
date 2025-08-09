fb8ecb50a6fcf7feb8126c7556100e00cb8b974c
Elias666-ap <eamayaparco@gmail.com>
2025-08-09 12:27:52 -0500

    Finalización de las funciones

--- test_anagrama.js
+++ test_anagrama.js
@@ -1,12 +1,30 @@
-const palabra = 
+let palabra = document.getElementById("palabra").value.trim().toLowerCase(); 
 
-function permutaciones(text){
-    return "";
+function permutaciones(str){
+    if(str.length <= 1)return [str];
+    let resultado = [];
+    for(let i = 0; i < str.length; i++){
+        let char = str[i];
+        let resto = str.slice(0, i) + str.slice(i + 1);
+        for (let subPerm of permutaciones(resto)){
+            resultado.push(char + subPerm);
+        }
+    }
+    return [...new set(resultado)];
+}
+//Api(Datamuse):
+
+let res = await fetch(`https://api.datamuse.com/words?sp=${palabra}&max=1`);
+let data = await res.json();
+
+
+//Funcion para encontrar solo palabras del diccionario:
+async function palabraValida(palabra){
+    return data.length > 0 && data[0].word.toLowerCase() === palabra.toLowerCase();
 }
 
-function palabraValida(){
-    return "";
-}
+//Formulario:
+
 
 
 
